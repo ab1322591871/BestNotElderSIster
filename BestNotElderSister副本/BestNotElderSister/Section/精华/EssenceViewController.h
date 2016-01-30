1c62592126046ba5e91227f4556eb1b331f45659
lanouhn <1322591871@qq.com>
2016-01-30 09:36:09 +0800

    no message

---
+++ BestNotElderSister副本/BestNotElderSister/Section/精华/EssenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  EssenceViewController.h
+//  BestNotElderSister
+//
+//  Created by lanouhn on 16/1/28.
+//  Copyright © 2016年 WangChuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EssenceViewController : UIViewController
+@property (nonatomic, strong) UIButton *titleButton;
+@end
